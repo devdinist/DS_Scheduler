7564595aafa4e311fc62ff220fe2ab52dba5e104
devdinist <dinist@naver.com>
2021-10-28 17:29:46 +0900

    v1.0.4

---
+++ plugin/Crawler_Detect/src/autoload.php
@@ -0,0 +1,6 @@
+<?php
+    require __DIR__.DIRECTORY_SEPARATOR."Fixtures".DIRECTORY_SEPARATOR."AbstractProvider.php";
+    require __DIR__.DIRECTORY_SEPARATOR."Fixtures".DIRECTORY_SEPARATOR."Crawlers.php";
+    require __DIR__.DIRECTORY_SEPARATOR."Fixtures".DIRECTORY_SEPARATOR."Exclusions.php";
+    require __DIR__.DIRECTORY_SEPARATOR."Fixtures".DIRECTORY_SEPARATOR."Headers.php";
+    require __DIR__.DIRECTORY_SEPARATOR."CrawlerDetect.php";
